ef16f447b8c0476766877fc07eb57bf22ec4548c
Sergiy Vlasov <sergeyvlasovthecreator@gmail.com>
2022-10-04 18:41:11 +0300

    file uploader as a reusable component

--- src/App.js
+++ src/App.js
@@ -2,24 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 import React, { useState } from "react";
+import FileUploader from "FileUploader";
 
 const App = () => {
   const [description, setDescription] = useState("");
   const [selectedFile, setSelectedFile] = useState(null);
+
   return (
       <div className="App">
         <form>
             <input
-                type="file"
-                value={selectedFile}
-                onChange={(e) => setSelectedFile(e.target.files[0])}
+                type="text"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
             />
 
-            <input
-              type="text"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-          />
+            <FileUploader
+                onFileSelectSuccess={(file) => setSelectedFile(file)}
+                onFileSelectError={({ error }) => alert(error)}
+            />
 
         </form>
       </div>
